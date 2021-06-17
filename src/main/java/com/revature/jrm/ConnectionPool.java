c1f35f58339809b28998fff298362fcbae25b268
ddinh <ddinh@users.noreply.github.com>
2021-06-17 13:46:55 -0500

    Split Model and fix connection pool

--- src/main/java/com/revature/jrm/ConnectionPool.java
+++ src/main/java/com/revature/jrm/ConnectionPool.java
@@ -11,23 +11,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ConnectionPool {
-    private static final ConnectionPool connection_factory = new ConnectionPool();
-    private BasicDataSource ds;
+    private static BasicDataSource ds;
 
     private static final Logger log = LoggerFactory.getLogger(ConnectionPool.class);
 
     static {
-        try {
-            Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException e) {
-            log.error("Failed to establish a connection with the Database: " + e);
-        }
-    }
-
-    /**
-     * private constructor for Utils.ConnectionFactory class.
-     */
-    ConnectionPool() {
         try {
             Properties props = new Properties();
             props.load(new FileReader("src/main/resources/application.properties"));
@@ -48,25 +36,11 @@ public class ConnectionPool {
     }
 
     /**
-     * Method to retrieve current static instance of Utils.ConnectionFactory class.
-     *
-     * @return current instance of Utils.ConnectionFactory object.
-     */
-    public static ConnectionPool getInstance() {
-        return connection_factory;
-    }
-
-    /**
      * Method to retrieve a connection to application database.
      *
      * @return Connection object.
      */
-    public Connection getConnection() {
-        try {
-            return ds.getConnection();
-        } catch (SQLException e) {
-            log.error("Failed to establish a connection with the Database: " + e);
-        }
-        return null;
+    public static Connection getConnection() throws SQLException {
+        return ds.getConnection();
     }
 }
--- src/main/java/com/revature/jrm/Model.java
+++ src/main/java/com/revature/jrm/Model.java
@@ -1,67 +1,6 @@
 package com.revature.jrm;
 
-import java.util.List;
-
 public abstract class Model<T> {
-    /**
-     * The connection pool used to get connections for the CRUD operations
-     */
-    private static ConnectionPool pool = new ConnectionPool();
-    /**
-     * The transaction manager is used to alter behavior if a transaction is in progress
-     */
-    private static TransactionManager transactionManager = new TransactionManager();
-
-    /**
-     * Returns an object from the database with the given id
-     *
-     * @param id the id of the entity
-     * @return the requested entity
-     */
-    public static <T> T get(int id) {
-        // 1. Use reflection API to get the table name from annotations
-        // 2. Get connection from connection pool
-        // 3. Query for entity using "where id=?"
-        // 4. Construct object and return it
-        return null;
-    }
-
-    /**
-     * Returns a list of all objects of the model's type
-     *
-     * @return the list of all objects
-     */
-    public static <T> List<T> all() {
-        // 1. Use reflection API to get the table name from annotations
-        // 2. Get connection from connection pool
-        // 3. Query for all entities
-        // 4. Create objects and return the list
-        return null;
-    }
-
-    /**
-     * Returns a list of objects that match the given "WHERE" clause
-     *
-     * @param query the value to put in the where clause
-     * @return list of objects that match
-     */
-    public static <T> List<T> where(String query) {
-        // 1. Use reflection API to get the table name from annotations
-        // 2. Get connection from connection pool
-        // 3. Build a select all query with query string parameter e.g. "select * from users where ?"
-        // 4. Create objects and return the list
-        return null;
-    }
-
-    /**
-     * Deletes all entities of the model's type
-     */
-    public static void destroy_all() {
-        // 1. Use reflection API to get the table name from annotations
-        // 2. Get connection from connection pool
-        // 3. Delete all objects e.g. "delete from users"
-    }
-
     /**
      * Saves the current object to database
      */
